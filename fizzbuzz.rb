f51134db49f368d2d33e1cf61356d77131bb31dd
Jared Velez <jared.zeth@gmail.com>
2020-08-25 14:13:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-fizzbuzz
-
+int % 3 == # if the number int is divisible by 3 "Fizz" # Go fizz
 
 end
